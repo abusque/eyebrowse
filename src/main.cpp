6cea5cbd2f6c5d481ec5fc792f9cfbd3bc6fe223
nic007 <nic007nc@gmail.com>
2013-10-05 19:07:24 -0400

    added the link for the user interface functions

--- src/main.cpp
+++ src/main.cpp
@@ -20,6 +20,7 @@ THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLI
 #include "constants.h"
 #include "findEyeCenter.h"
 #include "findEyeCorner.h"
+#include "uiActions.h"
 
 /** Function Headers */
 void detectAndDisplay( cv::Mat frame );
